bb6f5703ae2b92566e3d304693c3e859412239df
Benoit Asselin <contact@ab-d.fr>
2014-02-02 00:01:33 +0100

    no message

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-jquery-mousewheel-stop-propagation
-==================================
+$.mousewheelStopPropagation()
+=============================
 
 This jQuery plugin can prevent scrolling of parent element, or stop propagation with mousewheel event listener.
+
+*Compatibilities: Micorsoft Internet Explorer, Google Chrome, Mozilla Firefox, Apple Safari...*
+
+Usage
+-----
+
+```javascript
+<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+<script src="mousewheelStopPropagation.js"></script>
+<script>
+$(function(){
+	$('#my-element-with-overflow-auto').mousewheelStopPropagation();
+});
+</script>
+
+```
+
+Options
+-------
+
+| Name | Description | Type | Default |
+|------|-------------|------|---------|
+| `wheelstop` | The scroll was prevented. | Function | `null` |
+
---
+++ mousewheelStopPropagation.js
@@ -0,0 +1,74 @@
+/*!
+ * jquery.stopScrollingPropagation.js v1.0.0
+ * (c) 2014, Benoit Asselin contact(at)ab-d.fr
+ * MIT Licence
+ */
+ 
+;(function($, window, undefined) {
+	'use strict';
+	
+	$.fn.mousewheelStopPropagation = function(options) {
+		// defaults
+		options = $.extend({
+			wheelstop: null
+			}, options);
+		
+		var docElt = document.documentElement,
+		    mousewheelEventName = 'mousewheel';
+		if('onmousewheel' in docElt) {
+			mousewheelEventName = 'mousewheel';
+		} else if('onwheel' in docElt) {
+			mousewheelEventName = 'wheel';
+		} else if('DOMMouseScroll' in docElt) {
+			mousewheelEventName = 'DOMMouseScroll';
+		}
+		if(!mousewheelEventName) { return this; }
+		
+		var mousewheelPrevent = function(event) {
+			event.preventDefault();
+			event.stopPropagation();
+			if('function' === typeof options.wheelstop) {
+				options.wheelstop(event);
+			}
+		};
+		var isMsIE = ('Microsoft Internet Explorer' === navigator.appName);
+		
+		return this.each(function() {
+			var _this = this,
+			    $this = $(_this);
+			if(!isMsIE) {
+				$this.on(mousewheelEventName, function(event) {
+					var origiEvent = event.originalEvent;
+					var scrollTop = _this.scrollTop,
+					    scrollMax = _this.scrollHeight - $this.outerHeight(),
+					    delta = -origiEvent.wheelDelta;
+					if(isNaN(delta)) {
+						delta = origiEvent.deltaY;
+					}
+					if((scrollTop <= 0 && delta < 0) || (scrollTop >= scrollMax && delta > 0)) {
+						mousewheelPrevent(event);
+					}
+				});
+			} else {
+				$this.on(mousewheelEventName, function(event) {
+					var origiEvent = event.originalEvent;
+					var scrollTop = _this.scrollTop,
+					    scrollHeight = _this.scrollHeight,
+					    scrollMax = scrollHeight - $this.outerHeight(),
+					    delta = -origiEvent.wheelDelta;
+					if(-delta > scrollTop && delta < 0) {
+						$this.scrollTop(0);
+						mousewheelPrevent(event);
+					} else if(delta > scrollMax - scrollTop && delta > 0) {
+						//$this.scrollTop(scrollMax);
+						$this.scrollTop(scrollHeight);
+						mousewheelPrevent(event);
+					}
+				});
+			}
+		});
+	};
+	
+})(jQuery, window);
+
+
